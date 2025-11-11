eeffbecc727a7866622aa6cfb8c3d3a2c317e39f
adhiraj0905 <adhiraj0905@gmail.com>
2025-11-12 02:11:09 +0530

    added basic express server and nodemon

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+app.get('/', (req, res) => {
+  res.send('Hello World! This is the tubechat! backend server.');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running successfully on http://localhost:${port}`);
+});
