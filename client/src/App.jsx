1fc6e1ced3e296cace4130c9fd0d62abe922d8de
adhiraj0905 <adhiraj0905@gmail.com>
2025-11-12 09:04:59 +0530

    feat: Add frontend UI w glassmorphic i/p bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello World! This is the tubechat frontend.</h1>
+      <div className="main-container">
+        <hi className="title">TubeChat</hi>
+        <p className="subtitle">Chat with any youtube video.</p>
+        <form className="input-form">
+          <input
+            type="text"
+            className="url-input"
+            placeholder="Enter YouTube video URL"
+            />
+          <button type="submit" className="submit-button">Start Chatting</button>
+        </form>
+      </div>
     </div>
   );
 }
