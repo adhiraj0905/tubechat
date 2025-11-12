6bbca9763bb5279aa0a45d569d38784910192ae4
adhiraj0905 <adhiraj0905@gmail.com>
2025-11-12 08:33:52 +0530

    feat: Cleaned up react app and 'hello world'

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 app.get('/', (req, res) => {
-  res.send('Hello World! This is the tubechat! backend server.');
+  res.send('Hello World! This is the tubechat backend server.');
 });
 
 app.listen(port, () => {
